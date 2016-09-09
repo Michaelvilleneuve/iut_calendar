3f0e309f2a6e7f7793d8c4b127ace33d7caedd72
Michael Villeneuve <contact@michaelvilleneuve.fr>
2016-09-09 09:03:15 +0200

    Improved slug

--- app/models/source.rb
+++ app/models/source.rb
@@ -6,4 +6,8 @@ class Source < ActiveRecord::Base
     cal_file=(Net::HTTP.get "hyperplanning.iut.u-bordeaux.fr", url)
     Icalendar::Calendar.parse(cal_file)
   end
+
+  def to_param
+    "#{id}-#{title.parameterize}"
+  end 
 end
