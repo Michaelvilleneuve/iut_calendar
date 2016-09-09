ce2d6720a86b6b7c02d13e12bf82c8138d6baba5
Michael Villeneuve <contact@michaelvilleneuve.fr>
2016-09-09 08:32:56 +0200

    Refactored

--- app/controllers/events_controller.rb
+++
@@ -1,5 +0,0 @@
-class EventsController < ApplicationController
-  def index
-    @events = Event.where("starts_at >= ?", DateTime.now).order('starts_at asc')
-  end
-end
--- app/views/events/_event.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! event, :id, :title, :starts_at, :ends_at, :created_at, :updated_at
-json.url event_url(event, format: :json)
