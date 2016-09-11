972a2a57b2b18b71c90add23943ca397cc630132
Michael Villeneuve <contact@michaelvilleneuve.fr>
2016-09-11 11:24:55 +0200

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'sources#index'
-  resources :sources, only: [:index, :show]
+  resources :sources, only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
